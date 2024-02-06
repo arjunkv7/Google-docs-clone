ae601aed6cf8d5090595133f381e576ed8afc354
Arjun <arjunkvarju7@gmail.com>
2024-02-06 14:05:04 +0530

    Modified

--- server/lib/config/db.js
+++ server/lib/config/db.js
@@ -5,11 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectToDatabase = exports.JWT_KEY = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const MONGO_URL = "mongodb+srv://Arjunroot:TsK6It1f4HvMdGwM@cluster0.scesfd7.mongodb.net/googleDocs";
-exports.JWT_KEY = "th*&3oiudslkj&&^&#ljklfjsd";
+const MONGO_URL = process.env.DB_URL || "";
+exports.JWT_KEY = process.env.JWT_KEY || "";
+if (MONGO_URL == "") {
+    console.log('DB_URL is required in .env');
+    process.exit();
+}
 let connectToDatabase = () => {
+    console.log(MONGO_URL);
     mongoose_1.default.connect(MONGO_URL)
         .then(() => console.log('Database connected successfully...'))
-        .catch((err) => console.log("Error connecting to database: ", err.message));
+        .catch((err) => {
+        console.log("Error connecting to database: ", err.message);
+        process.exit();
+    });
 };
 exports.connectToDatabase = connectToDatabase;
